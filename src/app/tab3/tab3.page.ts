ce4446218d82f8b58e1c1f9663480b3e7aaa809a
Kelly Cardoso <kellycristinacardoso.713@gmail.com>
2023-03-17 20:39:00 -0300

    10

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+tamanho = 6;  
+valorAtual = 0;
 
+imgDado = [
+  'assets/dice-red.png', // Posição 0
+  'assets/dice.gif' // Posição 1
+];
+imagemDado = this.imgDado[0];
+
+constructor() {}
+
+jogarDado(){
+  this.valorAtual = 0;
+  this.imagemDado = this.imagemDado[1];
+
+  setTimeout(() => {
+   if (this.valorAtual = Math.floor(Math.random() * this.tamanho) + 1){
+     this.imagemDado[1]
+   } else {
+     this.imagemDado[0]
+   }
+  }, 2000);
+};
 }
