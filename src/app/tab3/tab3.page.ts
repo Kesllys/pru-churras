d7babe2256ed40d961c1b30c385a158c51c97ce5
Kelly Cardoso <kellycristinacardoso.713@gmail.com>
2023-03-17 21:07:26 -0300

    tab3 finalizado

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,33 +1,26 @@
 import { Component } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
+  tamanho = 6;
+  valorAtual = 0;
+  imgsDado = ['assets/dice-red.png', 'assets/dice.gif'];
 
-tamanho = 6;  
-valorAtual = 0;
+  imagemDado = this.imgsDado[0];
 
-imgDado = [
-  'assets/dice-red.png', // Posição 0
-  'assets/dice.gif' // Posição 1
-];
-imagemDado = this.imgDado[0];
+  constructor() {}
 
-constructor() {}
-
-jogarDado(){
-  this.valorAtual = 0;
-  this.imagemDado = this.imagemDado[1];
-
-  setTimeout(() => {
-   if (this.valorAtual = Math.floor(Math.random() * this.tamanho) + 1){
-     this.imagemDado[1]
-   } else {
-     this.imagemDado[0]
-   }
-  }, 2000);
-};
+  jogarDado() {
+    this.imagemDado = this.imgsDado[1];
+    this.valorAtual = 0;
+    setTimeout(() => {
+      let valorTemp = Math.floor(Math.random() * this.tamanho) + 1;
+      this.valorAtual = valorTemp < this.tamanho ? valorTemp : this.tamanho;
+    }, 2500);
+  }
 }
